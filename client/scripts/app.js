0fdeda056707c63fc783bf7c87d103869a1ab921
jqywang <jqywang.95@gmail.com>
2018-01-29 15:12:13 -0800

    Pass all tests for siskel and behave as intended

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -86,7 +86,7 @@ var MoviesView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
-    this.collection.on('sort');
+    this.collection.on('sort', this.render, this);
   },
 
   render: function() {
